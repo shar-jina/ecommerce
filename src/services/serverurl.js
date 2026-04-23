69d88987e9fcacb82b87627b8adc9cddad6e3fe4
shar-jina <sharjinaraseena@gmail.com>
2026-04-23 17:19:35 +0530

    add

--- src/services/serverurl.js
+++ src/services/serverurl.js
@@ -1,4 +1,4 @@
 // const SERVER_URL= "http://localhost:3000"
-const SERVER_URL= "https://ecommerce-fjnw.vercel.app"
+const SERVER_URL= "https://ecommerce-server-gphw.onrender.com"
 
 export default SERVER_URL
